62d2f69190bacdf7f2c512b7b683058cf9498475
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2025-04-17 21:42:14 +0500

    "code fix"

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -3,7 +3,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Collection;
 
 public interface FilmStorage {
-
     Film addFilm(Film film);
     Film updateFilm(Film film);
     Film getFilm(int id);
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,7 +3,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
-
     User createUser(User user);
     User updateUser(User user);
     User getUser(int id);
